2e9385a4857ba22d4ccdc06836854695a0e0a732
WRamos21 <ramossantoswesley@gmail.com>
2025-04-17 18:21:32 -0300

    Modificando bottom navigation para suportar edgeToEdge

--- app/src/main/java/com/example/gradeup/MainActivity.kt
+++ app/src/main/java/com/example/gradeup/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.gradeup
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,15 +26,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        supportActionBar?.hide()
+
+        setupNavigation()
+
+    }
+
+    private fun setupNavigation(){
+        val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
@@ -38,6 +47,5 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
     }
 }
