e38fe3ece3e72fc4a3149fd5b5fbb0db4f812bc8
WRamos21 <ramossantoswesley@gmail.com>
2025-05-13 18:26:01 -0300

    Layout da itemSubject finalizado
    Agora a seção de aula pratica ou teorica some dependendo se nao ha aula do tipo especifico

--- app/src/main/java/com/example/gradeup/ui/viewholder/SubjectsViewHolder.kt
+++ app/src/main/java/com/example/gradeup/ui/viewholder/SubjectsViewHolder.kt
@@ -1,15 +1,55 @@
 package com.example.gradeup.ui.viewholder
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gradeup.data.model.SubjectModel
 import com.example.gradeup.databinding.ItemSubjectBinding
 
-class SubjectsViewHolder(private val item: ItemSubjectBinding): RecyclerView.ViewHolder(item.root) {
+class SubjectsViewHolder(private val item: ItemSubjectBinding) :
+    RecyclerView.ViewHolder(item.root) {
 
-    fun bind(subject: SubjectModel){
+    fun bind(subject: SubjectModel) {
         item.textTitleSubject.text = subject.disciplina
-        item.textCampus.text = subject.campus
+        item.textCampus.text = buildString {
+            append(subject.turmaCodigo)
+            append(" | ")
+            append(subject.turno)
+            append(" | ")
+            append(subject.campus)
+        }
         item.textCourse.text = subject.curso
         item.textVacancies.text = subject.vagasTotais.toString()
+
+        item.textTheoryTeacher.text = (subject.docenteTeoria).replace("0", "")
+        item.textSchedulesTheory.text = subject.teoria.replace("; ", "\n")
+        item.textPraticeTeacher.text = (subject.docentePratica).replace("0", "")
+        item.textSchedulesPratice.text = subject.pratica.replace("; ", "\n")
+
+
+        if (subject.docenteTeoria == "0" && subject.teoria == "") {
+            item.textTheoryTeacher.visibility = View.GONE
+            item.textSchedulesTheory.visibility = View.GONE
+            item.textTheory.visibility = View.GONE
+            item.iconTheory.visibility = View.GONE
+
+        } else {
+            item.textTheoryTeacher.visibility = View.VISIBLE
+            item.textSchedulesTheory.visibility = View.VISIBLE
+            item.textTheory.visibility = View.VISIBLE
+            item.iconTheory.visibility = View.VISIBLE
+        }
+
+        if (subject.docentePratica == "0" && subject.pratica == "") {
+            item.textPraticeTeacher.visibility = View.GONE
+            item.textSchedulesPratice.visibility = View.GONE
+            item.textPratice.visibility = View.GONE
+            item.iconPratice.visibility = View.GONE
+        } else {
+            item.textPraticeTeacher.visibility = View.VISIBLE
+            item.textSchedulesPratice.visibility = View.VISIBLE
+            item.textPratice.visibility = View.VISIBLE
+            item.iconPratice.visibility = View.VISIBLE
+        }
     }
 }
+
