eb4a55749bfd543375f583d508d557d978a12665
WRamos21 <ramossantoswesley@gmail.com>
2025-06-21 20:43:47 -0300

    Finalizando a criacao da grade

--- app/src/main/java/com/example/gradeup/ui/viewholder/SelectedSubjectsViewHolder.kt
+++ app/src/main/java/com/example/gradeup/ui/viewholder/SelectedSubjectsViewHolder.kt
@@ -44,42 +44,30 @@ class SelectedSubjectsViewHolder(
         if (typeClass == "T") {
             item.textTheory.setTextColor(ContextCompat.getColor(itemView.context, R.color.primary))
             item.textPratice.setTextColor(ContextCompat.getColor(itemView.context, R.color.onSurface))
+            item.textPraticeTeacher.visibility = View.GONE
+            item.textPratice.visibility = View.GONE
+            item.iconPratice.visibility = View.GONE
+            item.textTheoryTeacher.visibility = View.VISIBLE
+            item.textTheory.visibility = View.VISIBLE
+            item.iconTheory.visibility = View.VISIBLE
         } else  {
             item.textPratice.setTextColor(ContextCompat.getColor(itemView.context, R.color.primary))
             item.textTheory.setTextColor(ContextCompat.getColor(itemView.context, R.color.onSurface))
-
+            item.textPraticeTeacher.visibility = View.VISIBLE
+            item.textPratice.visibility = View.VISIBLE
+            item.iconPratice.visibility = View.VISIBLE
+            item.textTheoryTeacher.visibility = View.GONE
+            item.textTheory.visibility = View.GONE
+            item.iconTheory.visibility = View.GONE
         }
-
+        item.textSchedulesPratice.visibility = View.GONE
+        item.textSchedulesTheory.visibility = View.GONE
 
         //Tramento de visbilidade
         toggleVisbility(subject)
     }
 
     private fun toggleVisbility(subject: SelectedSubjectEntity){
-        if (subject.docenteTeoria == "0" && subject.teoria == "") {
-            item.textTheoryTeacher.visibility = View.GONE
-            item.textSchedulesTheory.visibility = View.GONE
-            item.textTheory.visibility = View.GONE
-            item.iconTheory.visibility = View.GONE
-
-        } else {
-            item.textTheoryTeacher.visibility = View.VISIBLE
-            item.textSchedulesTheory.visibility = View.VISIBLE
-            item.textTheory.visibility = View.VISIBLE
-            item.iconTheory.visibility = View.VISIBLE
-        }
-
-        if (subject.docentePratica == "0" && subject.pratica == "") {
-            item.textPraticeTeacher.visibility = View.GONE
-            item.textSchedulesPratice.visibility = View.GONE
-            item.textPratice.visibility = View.GONE
-            item.iconPratice.visibility = View.GONE
-        } else {
-            item.textPraticeTeacher.visibility = View.VISIBLE
-            item.textSchedulesPratice.visibility = View.VISIBLE
-            item.textPratice.visibility = View.VISIBLE
-            item.iconPratice.visibility = View.VISIBLE
-        }
 
     }
 
